cef4c8bbb5858b14bdec053cfbe60492d64f4620
nkapusti <122452762+nkapusti@users.noreply.github.com>
2023-04-05 17:30:32 +0200

    Add files via upload

--- server.c
+++ server.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+/*#include "minitalk.h"
 
 static void	print_char(int sig)
 {
@@ -38,5 +38,85 @@ int	main(void)
 	signal(SIGUSR2, print_char);
 	while (1)
 		pause();
+	return (0);
+}*/
+
+#include "minitalk.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	num;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-", 1);
+		write(fd, "2", 1);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		num = n + '0';
+		write(fd, &num, 1);
+	}
+}
+
+void	handler(int signum, siginfo_t *info, void *context)
+{
+	static int	bytes;
+	static int	client_pid;
+	static char	c;
+
+	(void)context;
+	client_pid = info->si_pid;
+	if (signum == SIGUSR1)
+	{
+		c |= (0x01 << bytes);
+		bytes++;
+	}
+	else if (signum == SIGUSR2)
+		bytes++;
+	if (bytes == 8)
+	{
+		if (c == 0)
+			kill(client_pid, SIGUSR2);
+		else
+			write(1, &c, 1);
+		c = 0;
+		bytes = 0;
+	}
+	kill(client_pid, SIGUSR1);
+}
+
+int	main(int argc, char **argv)
+{
+	struct sigaction	sa;
+	int					pid1;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
+	(void)argv;
+	(void)argc;
+	pid1 = getpid();
+	sigemptyset(&sa.sa_mask);
+	ft_putnbr_fd(pid1, 1);
+	write(1, "\n", 1);
+	while (1)
+	{
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		pause();
+	}
 	return (0);
 }
